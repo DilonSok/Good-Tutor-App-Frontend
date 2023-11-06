1f87153a7d54b3cb7138198a5e471e14ac160cf5
Rushi1029 <61168374+Rushi1029@users.noreply.github.com>
2023-11-05 21:32:21 -0600

    add Sign Up pages - Rushi Kona

--- src/components/Signup-StudentPage.js
+++ src/components/Signup-StudentPage.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import '../css/signup.css';
+//Rushi Kona
 
 function SignupStudent() {
-  return <div><h1>Sign up as a Student</h1></div>;
+  return (
+    <div className='Signup'>
+        <div className='container'>
+            <div className="header">
+                <div className="text">Sign Up As Student</div>
+                <div className="underline"></div>
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    <input type="email" placeholder="Enter Email"/>
+                </div>
+                <div className="input">
+                    <input type="password" placeholder="Enter Password"/>
+                </div>
+                <div className="input">
+                    <input type="password" placeholder="Confirm Password"/>
+                </div>
+                <div className="input">
+                    <input type="Student First Name" placeholder="Enter Student First Name"/>
+                </div>
+                <div className="input">
+                    <input type="Student Last Name" placeholder="Enter Student Last Name"/>
+                </div>
+                <button className="submit-container" >Sign Up</button>
+            </div>
+        </div>
+    </div>
+  )
 }
 
 export default SignupStudent;
--- src/components/Signup-TutorPage.js
+++ src/components/Signup-TutorPage.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import '../css/signup.css';
+//Rushi Kona 
 
 function SignupTutor() {
-  return <div><h1>Sign up as a Tutor</h1></div>;
+  return (
+    <div className='Signup'>
+        <div className='container'>
+            <div className="header">
+                <div className="text">Sign Up As Tutor</div>
+                <div className="underline"></div>
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    <input type="email" placeholder="Enter Email"/>
+                </div>
+                <div className="input">
+                    <input type="password" placeholder="Enter Password"/>
+                </div>
+                <div className="input">
+                    <input type="password" placeholder="Confirm Password"/>
+                </div>
+                <div className="input">
+                    <input type="Tutor First Name" placeholder="Enter First Name"/>
+                </div>
+                <div className="input">
+                    <input type="Tutor Last Name" placeholder="Enter Last Name"/>
+                </div>
+                <div className="input">
+                    <input type="University" placeholder="Enter Current University"/>
+                </div>
+                <button className="submit-container" >Sign Up</button>
+            </div>
+        </div>
+    </div>
+  )
 }
 
 export default SignupTutor;
