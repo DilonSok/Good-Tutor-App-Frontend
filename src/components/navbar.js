b25b6edda938fa1405ee7f16eb259535dfd0c0d4
Dilon Sok <sokdilon@gmail.com>
2023-11-15 23:11:13 -0600

    Fixed navbar styling issue.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -55,7 +55,7 @@ function Navbar() {
       </div>
       <div ref={menuRef}>
         <Menu isOpen={isOpen}>
-          <ul>
+          <ul className='navlinks'>
             <li><NavLink to='/home'>Home</NavLink></li>
             <li><NavLink to='/profile'>Profile</NavLink></li>
             <li><NavLink to='/messages'>Messages</NavLink></li>
