cb09e449d86adcb5e6e1f720b02c3ee2ff0d55f9
Dilon Sok <sokdilon@gmail.com>
2023-11-17 00:39:49 -0600

    implemented state changes for logging and logging out
    should be okay??

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,38 +2,68 @@ import React, { useState, useEffect, useRef } from 'react';
 import logo from '../images/logo.jpg';
 import Menu from './menu';
 import '../css/navbar.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 //Dilon Sok
 //Frontend for navbar component
 // **dropdown menu items will be implemented at a later point and currently just proof of concept
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isOpen, setIsOpen] = useState(false); // States for the dropdown menu
   const menuRef = useRef(null);
   const profileIconRef = useRef(null);
+  const navigate = useNavigate();
 
-  //toggles the menu dropdown
+  //Function to logout user
+  const handleLogout = () => {
+    localStorage.setItem('isLoggedIn', 'false');
+    window.dispatchEvent(new Event('loginStateChange'));
+    navigate('/');
+  };
+
+  // Update login status function
+  const updateLoginStatus = () => {
+    const loggedInFlag = localStorage.getItem('isLoggedIn') === 'true';
+    setIsLoggedIn(loggedInFlag);
+  };
+
+  useEffect(() => {
+    updateLoginStatus();
+
+    const handleLoginStateChange = () => {
+      updateLoginStatus();
+    };
+
+    window.addEventListener('loginStateChange', handleLoginStateChange);
+
+    return () => {
+      window.removeEventListener('loginStateChange', handleLoginStateChange);
+    };
+  }, []);
+
+
+  //Listener for closing the menu when clicking outside of it
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && profileIconRef.current &&
+        !menuRef.current.contains(event.target) &&
+        !profileIconRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  // Toggles the menu dropdown
   const toggleMenu = (e) => {
     e.stopPropagation();
     setIsOpen(prevIsOpen => !prevIsOpen);
   };
-
-  //close the menu when clicking outside of it
-  const handleClickOutside = (event) => {
-    if (!menuRef.current.contains(event.target) && !profileIconRef.current.contains(event.target)) {
-      setIsOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    //attach the listener to the document
-    document.addEventListener('mousedown', handleClickOutside);
-    //clean up the event listener on component unmount
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []); 
-
   return (
     <div className="navbar">
       <div className="navbar-container">
@@ -46,25 +76,36 @@ function Navbar() {
           </li>
         </ul>
         <ul className='navbar-rightside'>
-          <li className='navbar-profile-logo' onClick={toggleMenu} ref={profileIconRef}>
-            <div className='navbar-profile-name'>
-              DS
-            </div>
-          </li>
+          {isLoggedIn ? (
+            // If logged in, show profile icon and menu
+            <>
+              <li className='navbar-profile-logo' onClick={toggleMenu} ref={profileIconRef}>
+                <div className='navbar-profile-name'>
+                  DS
+                </div>
+              </li>
+              <div ref={menuRef}>
+                <Menu isOpen={isOpen}>
+                  <ul className='navlinks'>
+                    <li><NavLink to='/home'>Home</NavLink></li>
+                    <li><NavLink to='/profile'>Profile</NavLink></li>
+                    <li><NavLink to='/messages'>Messages</NavLink></li>
+                    <li><NavLink to='/settings'>Settings</NavLink></li>
+                    <li onClick={handleLogout}>Logout</li>
+                  </ul>
+                </Menu>
+              </div>
+            </>
+          ) : (
+            // If not logged in, show Login and Sign Up buttons
+            <>
+              <li className='login-button'>
+                <NavLink to='/'>Login</NavLink>
+              </li>
+            </>
+          )}
         </ul>
       </div>
-      <div ref={menuRef}>
-        <Menu isOpen={isOpen}>
-          <ul className='navlinks'>
-            <li><NavLink to='/home'>Home</NavLink></li>
-            <li><NavLink to='/profile'>Profile</NavLink></li>
-            <li><NavLink to='/messages'>Messages</NavLink></li>
-            <li><NavLink to='/settings'>Settings</NavLink></li>
-            <li><NavLink to='/logout'>Logout</NavLink></li>
-          </ul>
-        </Menu>
-      </div>
-
     </div>
   );
 }
