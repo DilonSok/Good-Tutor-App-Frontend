201adb8fb4d443df96d9206578ff23b6dd69ac55
Dilon Sok <sokdilon@gmail.com>
2023-11-16 19:47:57 -0600

    fixed messages issue

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -4,23 +4,24 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function ProfilePage() {
   return (
-    
+
     <div className="profile-container">
-      <div className="messages-container">
-        <i className="fa-solid fa-comments fa-2x"></i>
-          <h3>
-            Message
-          </h3>
-      </div>
+
       <div className="profile-icon">
         <i className="fa-regular fa-circle-user fa-8x"></i>
       </div>
-      <h1> 
+      <h1>
         Tutor Name
       </h1>
       <h1>
         Schedule/Booking
       </h1>
+      <div className="message-button-container">
+        <i className="fa-solid fa-comments fa-2x"></i>
+          <h3>
+            Message
+          </h3>
+      </div>
     </div>
   );
 }
